cb8f526785251fe136c75a1a1b5d333faef59ce5
Jimmy Fish <killcoder212@gmail.com>
2024-03-11 22:52:32 +0700

    add redis and others

--- app/app.js
+++ app/app.js
@@ -5,10 +5,30 @@ import cron from 'cron';
 import BodyParser from 'body-parser';
 import Config from './configs/env.config';
 import Common from './routes/admin';
+import io from 'socket.io';
+import Cors from 'cors';
 
 const app = Express();
 app.use(BodyParser.urlencoded({ extended: true }))
 app.use(BodyParser.json());
+app.use(Cors());
+
+// io.on('connection', (socket) => {
+//   console.log('A user connected');
+
+//   // Handle incoming messages
+//   socket.on('message', (msg) => {
+//       console.log('Message received:', msg);
+
+//       // Broadcast the message to all connected clients
+//       io.emit('message-broadcast', msg);
+//   });
+
+//   // Handle disconnection
+//   socket.on('disconnect', () => {
+//       console.log('A user disconnected');
+//   });
+// });
 
 app.use('/', Common);
 
--- app/controllers/common/accountController.js
+++ app/controllers/common/accountController.js
@@ -1,3 +1,15 @@
-export async function doMe() {
-  console.log('Do Me please!');
+import { createClient } from 'redis';
+
+export async function doMe(req, res) {
+  const client = await createClient()
+    .on('error', err => console.log('Redis Client Error', err))
+    .connect();
+
+  const message = await client.get('notification');
+  console.log(message);
+
+  client.disconnect();
+  return res.status(200).send({
+    message: {},
+  });
 }
